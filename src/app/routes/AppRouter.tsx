424cfd0271bc365c1bbe5b077ddc3b348170c3be
nyoeng <hannayoung1210@gmail.com>
2026-09-09 21:21:27 +0900

    feat(routes): 준비중·CREDITS·플리 라우트 연결

--- src/app/routes/AppRouter.tsx
+++ src/app/routes/AppRouter.tsx
@@ -1,11 +1,16 @@
 import { Navigate, Route, Routes } from "react-router";
 
+import ComingSoonPage from "@/pages/coming-soon";
+import CreditsPage from "@/pages/credits";
 import HomePage from "@/pages/home";
 
 export const AppRouter = () => {
   return (
     <Routes>
       <Route index element={<HomePage />} />
+      <Route path="playlist" element={<HomePage />} />
+      <Route path="credits" element={<CreditsPage />} />
+      <Route path="coming-soon" element={<ComingSoonPage />} />
       <Route path="*" element={<Navigate replace to="/" />} />
     </Routes>
   );
